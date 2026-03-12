66034ec5079865c2d84cfaaa99e5b5c916cee256
jackc0re <chris.metodieff@gmail.com>
2026-03-12 18:21:19 +0200

    rose-pone-moon and supermaven
    Back to rose-pone-moon and added supermaven.

--- lua/plugins/gruvbox.lua
+++
@@ -1,9 +0,0 @@
--- lua/plugins/gruvbox.lua
-return {
-	"https://gitlab.com/motaz-shokry/gruvbox.nvim",
-	name = "gruvbox",
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme gruvbox")
-	end,
-}
