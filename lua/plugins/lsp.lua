f03f5364f3d0e4e9714a507acbeadac695d2d8ed
Chris Metodiev <chris.metodieff@gmail.com>
2026-03-12 19:18:33 +0200

    Revert "added markdown pretty printing"
    This reverts commit 55306f100c3990deec396361eba94d440ce1917c.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -206,9 +206,9 @@ return {
 			bashls = {},
 			marksman = {},
 			clangd = {},
-			-- gopls = {},
+			gopls = {},
 			pyright = {},
-			rust_analyzer = {},
+			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
